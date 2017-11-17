fa7fd953b572879322cdc37694f87b17d0cc2e6c
Sebastien Vallet <sebastien.vallet89@gmail.com>
2017-11-17 14:07:01 +0100

    Ajout de la vue veille

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -51,4 +51,15 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/veille", name="veille")
+     */
+    public function veilleAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('folio/veille.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
 }
